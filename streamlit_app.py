b129469ca31f9ce1be4444220b05e8f24b534e28
GusSLX <gusslx@darknite.localdomain>
2022-08-11 20:19:04 -0400

    Operators added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,47 @@
+import pandas as pd
 import streamlit as st
 
-st.write("Hello to the Mojix Bootcamp!")
+st.title('Welcome to my pandas basic tutorial 📊')
+
+st.image("pandas.gif")
+
+st.markdown("Here we will be discussing about pandas DataFrame Operations.")
+
+st.markdown("Our example dataset will be:")
+
+df = pd.DataFrame({
+      'A': [1, 2, 3, 4],
+      'B': [5, 6, 7, 8]
+    })
+
+st.write(df)
+
+st.markdown('First we can use common aritmethic operators between columns:')
+
+st.code("df['A'] + df['B']")
+st.write(df['A'] + df['B'])
+
+st.code("df['A'] - df['B']")
+st.write(df['A'] - df['B'])
+
+st.code("df['A'] * df['B']")
+st.write(df['A'] * df['B'])
+
+st.code("df['A'] / df['B']")
+st.write(df['A'] / df['B'])
+
+st.markdown('You can also use those aritmethic operators with constant variables:')
+
+value = st.slider('Pick a number', 0, 10)
+
+st.code("df['A'] + {}".format(value))
+st.write(df['A'] + value)
+
+st.code("df['A'] - {}".format(value))
+st.write(df['A'] - value)
+
+st.code("df['A'] * {}".format(value))
+st.write(df['A'] * value)
+
+st.code("df['A'] / {}".format(value))
+st.write(df['A'] / value)
